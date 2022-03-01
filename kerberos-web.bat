059198ba5d8c601f8d5e5d1a28217d45a0f3e10f
s0nnyhu <husonny1@gmail.com>
2022-03-02 00:07:19 +0100

    Enhance batch script

--- kerberos-web.bat
+++ kerberos-web.bat
@@ -1,3 +1,19 @@
+@echo off
+
+@REM CHECK IF APP DIR EXIST
+if NOT EXIST "C:\Kerberos\kerberos-web" (
+  echo "App directory might not exist"
+  exit /b 2
+)
+
+@REM CHECK IF NODE IS INSTALLED
+where node >nul 2>nul
+if %errorlevel%==1 (
+    @echo "Node might be not installed or not found in path (npm)"
+    exit
+)
+
+@REM RUN KERBEROS
 cd "C:\Kerberos\kerberos-web"
 CALL npm run start
 pause
